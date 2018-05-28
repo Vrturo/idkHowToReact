4897faec3b02c5c52e97b18862781567ca8b433c
Perez <perezart@amazon.com>
2018-05-28 14:35:41 -0700

    create html point of ref

---
+++ Toggle.react.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Toggle extends React.Component {
+  constructor(props) {
+    state = {
+      on: false,
+    }
+  }
+
+  render() {
+    <div> Toggle </div>
+  }
+}
