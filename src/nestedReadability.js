3cf115d432fb54adda628e7ffc932f48756a0b29
Perez <perezart@amazon.com>
2018-06-24 21:47:10 -0700

    add explanation

--- src/nestedReadability.js
+++ src/nestedReadability.js
@@ -1,5 +1,5 @@
 
-
+// Nested ternary statements are generally considered to be hard to maintain as their structure can be difficult to follow.
 const greeting = isJoiningRoom 
 	?	'Welcome'
 	: isLeavingRoom
@@ -8,7 +8,8 @@ const greeting = isJoiningRoom
 			? 'Welcome Back'
 			: 'Hi';
 
-
+// Using if-else statements for complex conditional assignments, it's much clear to read than the nested ternary statement. 
+// However in order to use this approach, we NEED to split the declaration of the variable greeting from its assignment/
 let greeting;
 if (isJoiningRoom) {
 	greeting = 'Welcome';
@@ -20,9 +21,28 @@ if (isJoiningRoom) {
 	greeting = 'Hi';
 }
 
-const greeting (() => {
+
+// Here, we have the same complex conditional assignment we had previously performed using an an immediately-invoked function expression, or IIFE. 
+// When we declare our variable greeting, we can assign its value to the result of the IIFE. 
+// We're able to maintain the readability of using if-else statements for conditional assignment 
+// without needing to split the declaration of the variable greeting from its assignment.
+
+const greeting = (() => {
 	if (isJoiningRoom) return 'Welcome';
 	if (isLeavingRoom) return 'Goodbye';
 	if (isReEnteringRoom) return 'Welcome Back';
 	return 'Hey there!'
-});
+})();
+
+
+
+const greeting = (() => {
+  const roomStatuses = [
+    [isJoiningRoom, 'Welcome'],
+    [isLeavingRoom, 'Goodbye'],
+    [isEnteringRoom, 'Welcome back'],
+    [true, 'Hi'],
+  ]
+  const roomStatus = roomStatuses.findIndex((element) => element[0])
+  return roomStatuses[roomStatus][1]
+})();
